28f240ceed7b1954dd9dfba04e219dac0d5dbf52
KasukabeYou <happy.lucky.blessing@gmail.com>
2020-09-16 12:42:40 +0000

    微調整

--- resources/views/member/edit.blade.php
+++ resources/views/member/edit.blade.php
@@ -11,6 +11,8 @@
                <input type="hidden" name="id" value="{{$member->id}}">
                <tr><th class="col-xs-2 p-2">name: </th><td class="col-xs-2 p-2"><input type="text" name="name" value="{{$member->name}}"></td></tr>
                <tr><th class="col-xs-2 p-2">email: </th><td class="col-xs-2 p-2"><input type="text" name="email" value="{{$member->email}}"></td></tr>
+               <input type="hidden" name="password" value="{{$member->password}}">
+               <input type="hidden" name="password_confirmation" value="{{$member->password}}">
                <tr><td class="col-xs-2 p-2 text-center" colspan="2"><input type="submit" value="edit"></td></tr>
         </table>
     </form>
