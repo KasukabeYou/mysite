e3048b69767df21777a4e47605bcd0ee3f76ab3f
KasukabeYou <happy.lucky.blessing@gmail.com>
2020-09-26 03:36:50 +0000

    Herokuへ編集

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,5 +30,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         $url->forceScheme('https');
+        Schema::defaultStringLength(191);
     }
 }
